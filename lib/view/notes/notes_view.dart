25dc084c1f2c7609b828b3df2730c4c985505d29
root <richkibz@gmail.com>
2023-01-17 19:11:51 +0300

    enable updating the notes

--- lib/view/notes/notes_view.dart
+++ lib/view/notes/notes_view.dart
@@ -36,7 +36,7 @@ class _NotesViewState extends State<NotesView> {
         actions: [
           IconButton(
               onPressed: () {
-                Navigator.of(context).pushNamed(newNoteRoute);
+                Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
               },
               icon: const Icon(Icons.add)),
           PopupMenuButton<MenuAction>(
@@ -82,8 +82,14 @@ class _NotesViewState extends State<NotesView> {
                         final allNotes = snapshot.data as List<DatabaseNotes>;
                         return NoteListView(
                           notes: allNotes,
-                          onDeleteNote: (note) async{
+                          onDeleteNote: (note) async {
                             await _notesService.deleteNote(id: note.id);
+                          },
+                          onTap: (note) {
+                            Navigator.of(context).pushNamed(
+                              createOrUpdateNoteRoute,
+                              arguments: note,
+                            );
                           },
                         );
                       } else {
